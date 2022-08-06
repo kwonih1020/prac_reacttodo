37dbc23aa51042028e042a0e853c7714132e4f2c
kwonih1020 <kwonih1020@gmail.com>
2022-08-07 00:19:34 +0900

    axios 적용, get 요청 성공

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 // src/App.js
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { __getTodos } from "./redux/modules/todosSlice";
 
 const App = () => {
-  // Store에 있는 todos 모듈 state 조회하기
-  const todos = useSelector((state) => state.todos.todos);
-  console.log(todos);
+  const { isLoading, error, todos } = useSelector((state) => state.todos);
+  // console.log(todos);
 
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(__getTodos());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>로딩 중....</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
   return (
     <div>
-      <input type="text" />
-      <input type="text" />
+      {todos.map((todo) => (
+        <div key={todo.id}>{todo.title}</div>
+      ))}
+      {/* <form type="submit" onSubmit={}> */}
+      <form type="submit">
+        <input type="text" placeholder="제목을 입력하시오" />
+        <input type="text" placeholder="내용을 입력하시오" />
+        <button>추가하기</button>
+      </form>
     </div>
   );
 };
